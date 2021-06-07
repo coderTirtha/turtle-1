dbedf5799044b790c87738e5402b65d294d02774
Tirthadas <75427198+tirthadas9775@users.noreply.github.com>
2021-06-07 21:01:45 +0600

    Add files via upload

---
+++ turtle love.py
@@ -0,0 +1,25 @@
+import turtle
+
+
+turtle.shape('turtle')
+turtle.speed(10)
+
+for i in range(63):
+    turtle.right(-30)
+    turtle.forward(30)
+    turtle.right(60)
+    turtle.forward(30)
+    turtle.right(60)
+    turtle.forward(30)
+    turtle.right(35)
+    turtle.forward(100)
+    turtle.right(110)
+    turtle.forward(95)
+    turtle.right(35)
+    turtle.forward(30)
+    turtle.right(60)
+    turtle.forward(30)
+    turtle.right(47)
+    turtle.forward(30)
+
+turtle.exitonclick()
---
+++ turtle star.py
@@ -0,0 +1,18 @@
+import turtle
+
+
+turtle.shape('turtle')
+turtle.speed(10)
+
+for i in range(84):
+    turtle.forward(150)
+    turtle.right(140)
+    turtle.forward(160)
+    turtle.right(145)
+    turtle.forward(165)
+    turtle.right(150)
+    turtle.forward(160)
+    turtle.right(147)
+    turtle.forward(150)
+
+turtle.exitonclick()
